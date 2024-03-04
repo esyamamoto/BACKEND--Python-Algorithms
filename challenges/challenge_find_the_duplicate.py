7f2a1ab1074b5c6164f223cddd14751195f6f308
esyamamoto <erika.s.yamamoto@gmail.com>
2024-03-04 20:38:50 -0300

    sinceramente? confuso

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str):
+        return False
+
+    if len(nums) < 2:
+        return False
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
