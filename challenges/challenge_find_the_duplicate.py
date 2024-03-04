fd093427d1e6fa9768e72986b0c1f277669f5a81
esyamamoto <erika.s.yamamoto@gmail.com>
2024-03-04 20:56:19 -0300

    blablabla mto complexo???

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,4 +1,7 @@
-def find_duplicate(nums):
+from re import T
+
+
+def validate(nums):
     if not nums or isinstance(nums, str):
         return False
 
@@ -8,8 +11,27 @@ def find_duplicate(nums):
     for num in nums:
         if not isinstance(num, int) or num < 0:
             return False
+    # is too complex blablabla
+    # nums.sort()
 
+    # for i in range(len(nums) - 1):
+    #    if nums[i] == nums[i + 1]:
+    #       return nums[i]
+
+    # return False
+    return True
+
+
+def sort_nums(nums):
     nums.sort()
+    return nums
+
+
+def find_duplicate(nums):
+    if not validate(nums):
+        return False
+
+    nums = sort_nums(nums)
 
     for i in range(len(nums) - 1):
         if nums[i] == nums[i + 1]:
